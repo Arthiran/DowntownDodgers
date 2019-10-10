d255ea10cb68eeb2d0568a0a7622c0bc2bf2800a
Arthiran Sivarajah <arthiran04@gmail.com>
2019-10-10 12:19:04 -0400

    Level Editor without Networking

---
+++ Assets/Scripts/CameraScriptNoNetwork.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CameraScriptNoNetwork : MonoBehaviour
+{
+    //Networking
+    private PhotonView PV;
+    //Get Character properties
+    public GameObject Player;
+    public GameObject PlayerEmptyChild;
+    public PlayerMovementControllerNoNetwork MovementController;
+
+    //Initialize Variables
+    public float mouseX;
+    public float mouseY;
+    public float RightAnalogX;
+    public float RightAnalogY;
+    public float inputSens = 150.0f;
+    [Range(0.0f, 1.0f)]
+    public float aimAssistFactor = 0.5f;
+    public float controllerInputSens = 150.0f;
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+    private float clampAngle = 90.0f;
+    private float distWall = 1f;
+    private string RightAnalogXString;
+    private string RightAnalogYString;
+
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        RightAnalogXString = "RightAnalogX1"; //+ GetComponentInParent<PlayerRootInfo>().PlayerID.ToString();
+        RightAnalogYString = "RightAnalogY1"; //+ GetComponentInParent<PlayerRootInfo>().PlayerID.ToString();
+        //Finds the character which has a tag set to Player
+        //Rotation variables are set
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        //Locks the mouse to the center of the screen and then hides it
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame, Camera movement should always be in LateUpdate()
+    private void Update()
+    {
+        //Gets mouse input and stores them in a variable
+        if (Cursor.visible == false)
+        {
+            mouseX = Input.GetAxis("Mouse X");
+            mouseY = Input.GetAxis("Mouse Y");
+            RightAnalogX = Input.GetAxis(RightAnalogXString);
+            RightAnalogY = Input.GetAxis(RightAnalogYString);
+        }
+        else if (Cursor.visible == true)
+        {
+            mouseX = 0f;
+            mouseY = 0f;
+            RightAnalogX = 0f;
+            RightAnalogY = 0f;
+        }
+
+        //Takes the mouse input and multiplies it by the sensitivity and then multiplied by Time.deltaTime for frame rate
+        rotY += mouseX * inputSens * Time.deltaTime;
+        rotX -= mouseY * inputSens * Time.deltaTime;
+
+        RaycastHit aimAssistRay;
+        if (Physics.Raycast(transform.position, transform.forward, out aimAssistRay, 1000))
+        {
+            if (aimAssistRay.collider.GetComponent<Target>() != null)
+            {
+                rotY += (RightAnalogX * controllerInputSens * Time.deltaTime) * aimAssistFactor;
+                rotX -= (RightAnalogY * controllerInputSens * Time.deltaTime) * aimAssistFactor;
+            }
+            else if (aimAssistRay.collider.GetComponent<Target>() == null)
+            {
+                rotY += RightAnalogX * controllerInputSens * Time.deltaTime;
+                rotX -= RightAnalogY * controllerInputSens * Time.deltaTime;
+            }
+        }
+        else
+        {
+            rotY += RightAnalogX * controllerInputSens * Time.deltaTime;
+            rotX -= RightAnalogY * controllerInputSens * Time.deltaTime;
+        }
+
+        //Clamps the rotation vertically so you can't view things upside down
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+
+        //Assigns the rotation to a variable and also sets a variable for the rotation of the player
+        //Actually changes the rotation
+        transform.eulerAngles = new Vector3(rotX, rotY, 0.0f);
+
+        PlayerEmptyChild.transform.eulerAngles = new Vector3(0.0f, rotY, 0.0f);
+        RaycastHit hit;
+        if (Physics.Raycast(Player.transform.position, PlayerEmptyChild.transform.transform.transform.forward, out hit, distWall) == false)
+        {
+            Player.transform.eulerAngles = new Vector3(0.0f, rotY, 0.0f);
+        }
+        else
+        {
+            if (hit.collider != null)
+            {
+                if (hit.collider.tag != "Climable")
+                {
+                    Player.transform.eulerAngles = new Vector3(0.0f, rotY, 0.0f);
+                }
+                else if (hit.collider.tag == "Climable" && MovementController.isClimbing == false)
+                {
+                    Player.transform.eulerAngles = new Vector3(0.0f, rotY, 0.0f);
+                }
+            }
+        }
+
+        //This is so that the Camera can follow the Player
+        transform.localPosition = new Vector3(Player.transform.localPosition.x, Player.transform.localPosition.y + 0.6f, Player.transform.localPosition.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementControllerNoNetwork.cs
@@ -0,0 +1,429 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class PlayerMovementControllerNoNetwork : MonoBehaviour
+{
+    //Get Character Properties
+    private Transform Player;
+    private CharacterController CharController;
+    public Camera PlayerCam;
+    public Image dashIconIMG;
+    public Image dashCooldownIMG;
+    public Image filledHealthbarIMG;
+    public Text dashCooldownText;
+    public Text interactText;
+    public Text HealthNumText;
+    public Text RespawningText;
+    private Shooting shootingScript;
+    private GameObject DodgeballInstance;
+    private Target PlayerHealth;
+    private GameObject[] SpawnPointList;
+
+    public GameObject DodgeballPrefab;
+    public Transform BallCarrierTransform;
+
+    private bool isDashCooldown;
+
+    //Initialize Variables
+    private float vertical;
+    private float horizontal;
+    private float LeftAnalogX;
+    private float LeftAnalogY;
+    private float velocityY;
+    private Vector3 movement;
+    private Vector3 currentImpact;
+    public float moveSpeed = 5f;
+    public float mass = 1f;
+    public float damping = 5f;
+    private float edgeUpForce = 15f;
+    public float climbSpeed = 100f;
+    public float jumpForce = 4f;
+    public float dashForce = 4f;
+    public float dashCooldown = 5f;
+    private float dashCountdownUI;
+    private float nextDash;
+    private float gravity = Physics.gravity.y;
+    private float distWall = 0.6f;
+    public bool isClimbing = false;
+    private bool isFalling = false;
+    private bool isRespawning = false;
+    public bool inHand = true;
+    private bool inHandStored = false;
+    public float climbWallTimer = 1f;
+    private float tempClimbTimer;
+    private float respawnTimer = 1f;
+    private float tempRespawnTimer;
+    public int tempRandomNum;
+    private int PlayerID;
+
+    private float forwardMovement;
+    private float horizontalMovement;
+
+    private int doubleJumpCheck = 0;
+
+    //ControllerStrings
+    private string LeftAnalogXString;
+    private string LeftAnalogYString;
+    private string ControllerJumpString;
+    private string ControllerDashString;
+    private string ControllerLoadString;
+    private string ControllerInteractString;
+
+    private void Start()
+    {
+        PlayerCam.gameObject.SetActive(true);
+
+        //Set Controller Strings
+        LeftAnalogXString = "LeftAnalogX1"; //+ PlayerID.ToString();
+        LeftAnalogYString = "LeftAnalogY1"; //+ PlayerID.ToString();
+        ControllerJumpString = "ControllerJump1"; //+ PlayerID.ToString();
+        ControllerDashString = "ControllerDash1"; //+ PlayerID.ToString();
+        ControllerLoadString = "ControllerLoad1"; //+ PlayerID.ToString();
+        ControllerInteractString = "ControllerInteract1"; //+ PlayerID.ToString();
+        SpawnPointList = GameObject.FindGameObjectsWithTag("PlayerSpawn");
+
+        //Set Components to Variables at Start of Script
+        Player = GetComponent<Transform>();
+        CharController = GetComponent<CharacterController>();
+        shootingScript = GetComponentInParent<Shooting>();
+        PlayerHealth = GetComponent<Target>();
+
+        tempClimbTimer = climbWallTimer;
+        tempRespawnTimer = respawnTimer;
+
+        dashCountdownUI = dashCooldown + 1;
+    }
+
+    //Any Input(Keyboard or Mouse) should be in Update function
+    private void Update()
+    {
+        //Checks if Player is on the ground, if true set Y Velocity to 0
+        if (CharController.isGrounded && velocityY < 0f)
+        {
+            doubleJumpCheck = 0;
+            isClimbing = false;
+            isFalling = false;
+            //inHandStored = inHand;
+            tempClimbTimer = climbWallTimer;
+            tempRespawnTimer = respawnTimer;
+            velocityY = 0f;
+        }
+
+        //Get Input from Horizontal and Vertical Axis and store them in variables
+        if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            vertical = Input.GetAxisRaw("Vertical");
+            forwardMovement = vertical;
+        }
+        else
+        {
+            LeftAnalogY = Input.GetAxisRaw(LeftAnalogYString);
+            forwardMovement = LeftAnalogY;
+        }
+
+        if (isClimbing == false)
+        {
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                horizontal = Input.GetAxisRaw("Horizontal");
+                horizontalMovement = horizontal;
+            }
+            else
+            {
+                LeftAnalogX = Input.GetAxisRaw(LeftAnalogXString);
+                horizontalMovement = LeftAnalogX;
+            }
+            Dash();
+        }
+
+        /*This is so that when you press W and A at the same time for instance, the player doesn't become faster,
+        it remains the same speed*/
+        if (forwardMovement != 0 && horizontalMovement != 0)
+        {
+            forwardMovement *= 0.7071f;
+            horizontalMovement *= 0.7071f;
+        }
+
+        //Jump pls
+        Jump();
+        ClimbWall();
+        LoadDodgeball();
+
+        if (isDashCooldown)
+        {
+            dashIconIMG.color = new Color(dashIconIMG.color.r, dashIconIMG.color.g, dashIconIMG.color.b, 1f);
+            dashCooldownIMG.fillAmount += 1 / dashCooldown * Time.deltaTime;
+            dashCountdownUI -= Time.deltaTime;
+            if (dashCooldownIMG.fillAmount >= 1)
+            {
+                dashCountdownUI = 0;
+                dashCooldownIMG.fillAmount = 0;
+                dashCountdownUI = dashCooldown + 1;
+                dashCooldownText.text = "";
+                isDashCooldown = false;
+            }
+            else
+            {
+                dashIconIMG.color = new Color(dashIconIMG.color.r, dashIconIMG.color.g, dashIconIMG.color.b, 0.1f);
+                dashCooldownText.text = dashCountdownUI.ToString();
+            }
+        }
+    }
+
+    //All Physics and Movement should be handled in FixedUpdate()
+    private void FixedUpdate()
+    {
+        //Create a Vector to store the overall movement
+        movement = new Vector3(horizontalMovement, 0, forwardMovement);
+        Vector3 velocity;
+
+        /*Takes the movement vector and converts the position from Local Space to World Space and stores 
+        it back in the movement variable*/
+        movement = transform.TransformDirection(movement);
+
+        //Calculates gravity and stores it in variable
+        velocityY += gravity * Time.deltaTime;
+
+        //Vector which stores the overall effect of gravity on the Character's position
+        if (inHand == true)
+        {
+            velocity = movement * moveSpeed + Vector3.up * velocityY;
+        }
+        else
+        {
+            velocity = movement * moveSpeed * 1.5f + Vector3.up * velocityY;
+        }
+
+        //sets the velocity to take all forces into account
+        if (currentImpact.magnitude > 0.2f)
+        {
+            velocity += currentImpact;
+        }
+
+        if (DodgeballInstance != null)
+        {
+            DodgeballInstance.transform.position = BallCarrierTransform.position;
+        }
+
+        //Takes the velocity and actually moves the Character
+        if (isRespawning == false)
+        {
+            CharController.Move(velocity * Time.fixedDeltaTime);
+        }
+
+        //Interpolates the effects of forces for smooth movement
+        currentImpact = Vector3.Lerp(currentImpact, Vector3.zero, damping * Time.deltaTime);
+    }
+
+    //Resets all forces
+    private void ResetImpact()
+    {
+        currentImpact = Vector3.zero;
+        velocityY = 0f;
+    }
+
+    //Resets forces on the Y axis
+    private void ResetImpactY()
+    {
+        currentImpact.y = 0f;
+        velocityY = 0f;
+    }
+
+    //Jump Function
+    private void Jump()
+    {
+        //Checks if Space was pressed
+        if (Input.GetButtonDown("Jump") || Input.GetButtonDown(ControllerJumpString))
+        {
+            doubleJumpCheck++;
+            //Checks if player is on the ground, if true then character can jump
+            if (doubleJumpCheck <= 2)
+            {
+                //Custom AddForce function which applies jumpForce in the upward direction
+                AddForce(Vector3.up, jumpForce);
+                if (doubleJumpCheck == 2)
+                {
+                    AddForce(Vector3.up, jumpForce * 1.5f);
+                }
+            }
+        }
+    }
+
+    private void Dash()
+    {
+        if ((Input.GetKeyDown(KeyCode.LeftShift) || Input.GetAxisRaw(ControllerDashString) > 0) && Time.time > nextDash)
+        {
+            isDashCooldown = true;
+            nextDash = Time.time + dashCooldown;
+            AddForce(PlayerCam.transform.forward, dashForce);
+        }
+    }
+
+    private void LoadDodgeball()
+    {
+        if (Input.GetKeyDown(KeyCode.R) || Input.GetButtonDown(ControllerLoadString))
+        {
+            if (shootingScript.DodgeballsInHand != 0 && inHand == false)
+            {
+                inHand = true;
+                DodgeballInstance = Instantiate(DodgeballPrefab, BallCarrierTransform.position, BallCarrierTransform.rotation);
+            }
+            else if (shootingScript.DodgeballsInHand != 0 && inHand == true)
+            {
+                inHand = false;
+                inHandStored = inHand;
+                Destroy(DodgeballInstance);
+            }
+        }
+        else if (shootingScript.DodgeballsInHand == 0)
+        {
+            if (inHand == false)
+            {
+                if (DodgeballInstance != null)
+                {
+                    Destroy(DodgeballInstance);
+                }
+            }
+            else if (inHand == true)
+            {
+                if (inHandStored != true)
+                {
+                    inHandStored = inHand;
+                    inHand = false;
+                }
+                inHand = false;
+            }
+        }
+        else if (shootingScript.DodgeballsInHand != 0)
+        {
+            if (DodgeballInstance == null && inHandStored == true && isClimbing == false)
+            {
+                DodgeballInstance = Instantiate(DodgeballPrefab, BallCarrierTransform.position, BallCarrierTransform.rotation);
+                inHand = true;
+            }
+            else if (DodgeballInstance != null && inHand == false)
+            {
+                Destroy(DodgeballInstance);
+            }
+        }
+    }
+
+    //Custom AddForce function, not to be mistakened with Rigidbody.AddForce()
+    public void AddForce(Vector3 direction, float magnitude)
+    {
+        //Adds the force to the current amount of forces being applied to the game object
+        currentImpact += direction.normalized * magnitude / mass;
+    }
+
+    private void ClimbWall()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distWall))
+        {
+            if (hit.collider.tag == "Climable")
+            {
+                if ((Input.GetAxisRaw("Vertical") > 0 || Input.GetAxisRaw(LeftAnalogYString) > 0) && (Input.GetButton("Jump") || Input.GetButton(ControllerJumpString)) && tempClimbTimer > 0)
+                {
+                    tempClimbTimer -= Time.deltaTime;
+                    ResetImpactY();
+                    //Gives the Character movement on the Y axis to climb up the wall
+                    CharController.Move(new Vector3(0f, climbSpeed * Time.deltaTime, 0f));
+                    isClimbing = true;
+                    if (inHand == true)
+                    {
+                        inHandStored = inHand;
+                        inHand = false;
+                    }
+                }
+                else if (tempClimbTimer <= 0)
+                {
+                    isClimbing = false;
+                    isFalling = true;
+                }
+                else
+                {
+                    isClimbing = false;
+                }
+            }
+        }
+        else if (isClimbing == true && isFalling == false && CharController.isGrounded == false)
+        {
+            //Resets Forces on the Y axis
+            ResetImpactY();
+            //This is to nudge the character above the ledge of the top of the wall
+            AddForce(Vector3.up, edgeUpForce);
+            tempClimbTimer = climbWallTimer;
+            isClimbing = false;
+            isFalling = false;
+            if (inHandStored)
+            {
+                inHand = true;
+            }
+        }
+        else
+        {
+            tempClimbTimer = climbWallTimer;
+        }
+
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Loot")
+        {
+            if (interactText.color.a != 1 && shootingScript.DodgeballsInHand != shootingScript.DodgeballCarryLimit)
+            {
+                interactText.color = new Color(interactText.color.r, interactText.color.g, interactText.color.b, 1f);
+            }
+            if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown(ControllerInteractString))
+            {
+                if (shootingScript.DodgeballsInHand < shootingScript.DodgeballCarryLimit)
+                {
+                    shootingScript.DodgeballsInHand++;
+                    Destroy(collider.gameObject);
+                }
+                if (interactText.color.a != 0)
+                {
+                    interactText.color = new Color(interactText.color.r, interactText.color.g, interactText.color.b, 0f);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Loot")
+        {
+            if (interactText.color.a != 0)
+            {
+                interactText.color = new Color(interactText.color.r, interactText.color.g, interactText.color.b, 0f);
+            }
+        }
+    }
+
+    public IEnumerator Respawn()
+    {
+        isRespawning = true;
+        RespawningText.color = new Color(RespawningText.color.r, RespawningText.color.g, RespawningText.color.b, 1f);
+        tempRandomNum = Random.Range(0, SpawnPointList.Length - 1);
+        gameObject.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<CharacterController>().enabled = false;
+        yield return new WaitForSeconds(3);
+        transform.position = SpawnPointList[tempRandomNum].transform.position;
+        transform.eulerAngles = SpawnPointList[tempRandomNum].transform.eulerAngles;
+        isRespawning = false;
+        shootingScript.DodgeballsInHand = 0;
+        nextDash = nextDash - dashCooldown;
+        isDashCooldown = true;
+        dashCooldownIMG.fillAmount = 1f;
+        RespawningText.color = new Color(RespawningText.color.r, RespawningText.color.g, RespawningText.color.b, 0f);
+        HealthNumText.text = PlayerHealth.originalHealth.ToString() + "/" + PlayerHealth.originalHealth.ToString();
+        filledHealthbarIMG.fillAmount = PlayerHealth.originalHealth / PlayerHealth.originalHealth;
+        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        gameObject.GetComponent<CharacterController>().enabled = true;
+    }
+}
