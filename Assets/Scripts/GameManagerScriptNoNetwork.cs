4d1ea219e77c9d02b3af6bf1313726be41150f24
Gil <gil.robern@ontariotechu.net>
2019-11-24 15:09:03 -0500

    End Conditions WIP
    The game now ends after timer runs out (timer not visible yet)

--- Assets/Scripts/GameManagerScriptNoNetwork.cs
+++ Assets/Scripts/GameManagerScriptNoNetwork.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class GameManagerScriptNoNetwork : MonoBehaviour
 {
@@ -14,10 +15,24 @@ public class GameManagerScriptNoNetwork : MonoBehaviour
 
     public GameObject DodgeballLootPrefab;
     public Collider TempSpawnBox;
+
+    public int p1Score = 0;
+    public int p2Score = 0;
+
+    private float gameTime = 10.0f;
+
+    public Text p1Result;
+    public Text p2Result;
     // Start is called before the first frame update
     void Start()
     {
         SpawnPointList = GameObject.FindGameObjectsWithTag("PlayerSpawn");
+
+        p1Result = p1Result.GetComponent<Text>();
+        p2Result = p2Result.GetComponent<Text>();
+
+        p1Result.enabled = false;
+        p2Result.enabled = false;
 
         /*for (int i = 0; i <= NumberOfPlayers - 1; i++)
         {
@@ -35,6 +50,30 @@ public class GameManagerScriptNoNetwork : MonoBehaviour
 
     private void Update()
     {
+        gameTime -= Time.deltaTime;
+
+        if (gameTime <= 0.0f)
+        {
+            p1Result.enabled = true;
+            p2Result.enabled = true;
+
+            if (p1Score > p2Score)
+            {  
+                p1Result.text = "You Win!";
+                p2Result.text = "You Lose!";
+            }
+            else if (p2Score > p1Score)
+            {
+                p1Result.text = "You Lose!";
+                p2Result.text = "You Win!";
+            }
+            else
+            {
+                p1Result.text = "Tie Game!";
+                p2Result.text = "Tie Game!";
+            }
+        }
+
         if (GameObject.FindGameObjectsWithTag("Loot").Length < 3)
         {
             SpawnNewDodgeball();
