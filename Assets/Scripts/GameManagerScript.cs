ac665ea9ab0a9aae4a3e7a5e89579b6f36ba0d8d
Arthiran Sivarajah <arthiran04@gmail.com>
2020-01-21 13:04:38 -0500

    Removed Stuff
    Removed level editor
    Removed Score

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -16,19 +16,11 @@ public class GameManagerScript : MonoBehaviour
     public GameObject DodgeballLootPrefab;
 
     private int LootRandomNum = 0;
-    public int p1Score = 0;
-    public int p2Score = 0;
 
     private float gameTime = 180.0f;
 
-    public Text p1ResultText;
-    public Text p2ResultText;
-
     public Text timerText1;
     public Text timerText2;
-
-    public Text scoreText1;
-    public Text scoreText2;
 
     public float seconds;
     public float minutes;
@@ -45,21 +37,9 @@ public class GameManagerScript : MonoBehaviour
 
         gameOver = false;
 
-        //Set scores to start at 0
-        p1Score = 0;
-        p2Score = 0;
-
         //Initialize UI elements
-        p1ResultText = p1ResultText.GetComponent<Text>();
-        p2ResultText = p2ResultText.GetComponent<Text>();
         timerText1 = timerText1.GetComponent<Text>();
         timerText2 = timerText2.GetComponent<Text>();
-        scoreText1 = scoreText1.GetComponent<Text>();
-        scoreText2 = scoreText2.GetComponent<Text>();
-
-        //Hide the text
-        p1ResultText.enabled = false;
-        p2ResultText.enabled = false;
     }
 
     private void Update()
@@ -82,13 +62,9 @@ public class GameManagerScript : MonoBehaviour
             timerText2.text = minutes.ToString("00") + ":" + seconds.ToString("00");
         }     
 
-        //Display Score
-        scoreText1.text = p1Score.ToString("0");
-        scoreText2.text = p2Score.ToString("0");
-
 
         //End Conditions
-        if (gameTime <= 0.0f || p1Score == 3 || p2Score == 3)
+        if (gameTime <= 0.0f)
         {
             //timerText1.text = "00" + ":" + "00";
             //timerText2.text = "00" + ":" + "00";
@@ -97,29 +73,6 @@ public class GameManagerScript : MonoBehaviour
             {
                 endTime = gameTime;
                 gameOver = true;
-            }
-
-            //Make text visible
-            p1ResultText.enabled = true;
-            p2ResultText.enabled = true;
-
-            //P1 wins
-            if (p1Score > p2Score)
-            {  
-                p1ResultText.text = "You Win!";
-                p2ResultText.text = "You Lose!";
-            }
-            //P2 wins
-            else if (p2Score > p1Score)
-            {
-                p1ResultText.text = "You Lose!";
-                p2ResultText.text = "You Win!";
-            }
-            //Tie game
-            else
-            {
-                p1ResultText.text = "Tie Game!";
-                p2ResultText.text = "Tie Game!";
             }
 
             if (gameTime < endTime - 3.0f)
--- Assets/Scripts/SceneInitializerScript.cs
+++ Assets/Scripts/SceneInitializerScript.cs
@@ -5,38 +5,12 @@ using UnityEngine;
 
 public class SceneInitializerScript : MonoBehaviour
 {
-    private int readParameters = 10;
-
     public Material skybox1, skybox2;
 
     public List<Light> lights = new List<Light>();
 
     public GameObject bulb;
     public Material bulbOff, bulbOn;
-
-    public List<GameObject> spawnablePrefabs = new List<GameObject>();
-
-    private const string DLL_NAME = "LevelEditorPlugin";
-
-    private string folderLocation = Application.streamingAssetsPath + "/";
-    [SerializeField]
-    private string levelName = "default";
-    private const string textExtension = ".txt";
-    private string fileToSave;
-
-    private bool finishedLoading = false;
-
-    [DllImport(DLL_NAME)]
-    private static extern void startWriting(string fileName);
-    [DllImport(DLL_NAME)]
-    private static extern void endWriting();
-    [DllImport(DLL_NAME)]
-    private static extern void ReadInto(float objectNumber, float locationx, float locationy, float locationz, float rotationx,
-    float rotationy, float rotationz, float scalex, float scaley, float scalez);
-    [DllImport(DLL_NAME)]
-    private static extern float ReadOut(int j, string fileName);
-    [DllImport(DLL_NAME)]
-    private static extern int returnLines(string fileName);
 
     private void Start()
     {
@@ -53,31 +27,6 @@ public class SceneInitializerScript : MonoBehaviour
             foreach (var light in lights)
                 light.enabled = true;
             bulb.transform.Find("default").GetComponent<MeshRenderer>().material = bulbOn;
-        }
-        fileToSave = folderLocation + levelName + textExtension;
-        loadObjects();
-    }
-
-    public void loadObjects()
-    {
-        finishedLoading = true;
-        int objectSet = 0;
-
-        while (finishedLoading)
-        {
-            for (int i = 0; i <= (returnLines(fileToSave) / readParameters); i++)
-            {
-                GameObject tempSpawnableObject;
-                tempSpawnableObject = Instantiate(spawnablePrefabs[Mathf.RoundToInt(ReadOut(0 + objectSet, fileToSave))], new Vector3(ReadOut(1 + objectSet, fileToSave), ReadOut(2 + objectSet, fileToSave), ReadOut(3 + objectSet, fileToSave)), Quaternion.Euler(ReadOut(4 + objectSet, fileToSave), ReadOut(5 + objectSet, fileToSave), ReadOut(6 + objectSet, fileToSave)));
-                tempSpawnableObject.transform.localScale = new Vector3(ReadOut(7 + objectSet, fileToSave), ReadOut(8 + objectSet, fileToSave), ReadOut(8 + objectSet, fileToSave));
-                objectSet = objectSet + readParameters;
-                if (objectSet == returnLines(fileToSave))
-                {
-                    finishedLoading = false;
-                    break;
-                }
-            }
-            finishedLoading = false;
         }
     }
 }
