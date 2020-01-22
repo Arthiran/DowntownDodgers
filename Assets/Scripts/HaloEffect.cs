b4cee43220a4aecb41aa1068c42eefc91a08b0b1
Gil Robern <gil.robern@uoit.net>
2020-01-22 08:35:42 -0500

    More dodgeball particle updates
    The ball also floats in the air!

--- Assets/Scripts/HaloEffect.cs
+++ Assets/Scripts/HaloEffect.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class HaloEffect : MonoBehaviour
 {
     public Vector3 speed;
+    public float amplitude;
+    public float frequency;
+
+    private Vector3 posOffset = new Vector3();
+    private Vector3 tempPos = new Vector3();
     // Start is called before the first frame update
     void Start()
     {
-        
+        posOffset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(speed * Time.deltaTime);
+
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+
+        transform.position = tempPos;
     }
 }
