cf31f9594b44900962e4406d09bb583c2f2ba7ab
Gil Robern <gil.robern@uoit.net>
2020-01-24 09:12:19 -0500

    Some small changes before review
    New dodgeball temp texture
    new lootspawn placements

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -81,7 +81,7 @@ public class GameManagerScript : MonoBehaviour
             }
         }
 
-        if (GameObject.FindGameObjectsWithTag("Loot").Length < 3)
+        if (GameObject.FindGameObjectsWithTag("Loot").Length < 6)
         {
             SpawnNewDodgeball();
         }
