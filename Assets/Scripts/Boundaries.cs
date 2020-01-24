83f849093065a9941f741eb299bc6cd1b0145cb6
Gil Robern <gil.robern@uoit.net>
2020-01-23 23:46:29 -0500

    Some time wasted... New building placed to demo
    RIP to Jess and I

---
+++ Assets/Scripts/Boundaries.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+
+    public float killTimer = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < -5.2f)
+        {
+            killTimer -= Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            killTimer = 5.0f;
+        }
+    }
+}
