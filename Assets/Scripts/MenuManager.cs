baaf588aad839067172485b9a966e0140feaab52
Gil Robern <gil.robern@uoit.net>
2020-02-13 23:41:26 -0500

    A bunch of models added
    Also updated some menu things

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+    public Image instructions;
+    public Button yaReady;
+
     // Start is called before the first frame update
     void Start()
     {
+        instructions = instructions.GetComponent<Image>();
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
@@ -15,6 +20,12 @@ public class MenuManager : MonoBehaviour
     public void LoadLevel(string levelName)
     {
         SceneManager.LoadScene(levelName);
+    }
+
+    public void DisplayInstructions()
+    {
+        instructions.enabled = true;
+        yaReady.image.enabled = true;
     }
 
 	public void QuitGame()
