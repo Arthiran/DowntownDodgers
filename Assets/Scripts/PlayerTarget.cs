455da675543847ce2e227c3af8c7e105bd14ba18
Gil Robern <gil.robern@uoit.net>
2020-02-29 22:31:51 -0500

    Highlight/Crosshair Colour Change WIP
    I dont understand how raycast works :/

---
+++ Assets/Scripts/PlayerTarget.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerTarget : MonoBehaviour
+{
+    public Image reticle;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reticle = reticle.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit, 1000.0f))
+        {
+            if (hit.transform.gameObject.name == "Player")
+            {
+                Debug.Log("Hit");
+                reticle.color = Color.red;
+            }
+            else
+            {
+                reticle.color = new Color(0.0f, 255.0f, 52.0f);
+            }
+        }
+        else
+        {
+            reticle.color = new Color(0.0f, 255.0f, 52.0f);
+        }
+    }
+}
