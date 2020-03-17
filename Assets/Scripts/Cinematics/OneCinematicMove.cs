c72e2ebdc4eba0a1f68ac92f2eb5fe37f0a56f6f
Jessica Le <jessicale422@gmail.com>
2020-03-17 13:01:04 -0400

    Cinematics

---
+++ Assets/Scripts/Cinematics/OneCinematicMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneCinematicMove : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.transform.Translate(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.transform.Translate(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.transform.Translate(1, 0, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Cinematics/TwoCinematicMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoCinematicMove : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.T))
+        {
+            player.transform.Translate(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.G))
+        {
+            player.transform.Translate(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.F))
+        {
+            player.transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.H))
+        {
+            player.transform.Translate(1, 0, 0);
+        }
+    }
+}
