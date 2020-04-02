d018d5d23d89badceb916ecd321ebe5935ad4c1b
Arthiran Sivarajah <arthiran04@gmail.com>
2020-04-01 22:49:19 -0400

    Controller with Menu works

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -8,16 +8,33 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
     public Image instructions;
+    public GameObject selectedObject;
     public Button yaReady;
-    public Button FirstBtn;
+    public GameObject FirstBtn;
+    private GameObject previousBtn;
     public EventSystem es;
 
     // Start is called before the first frame update
     void Start()
     {
         instructions = instructions.GetComponent<Image>();
-        es.SetSelectedGameObject(null);
         es.SetSelectedGameObject(FirstBtn.gameObject);
+        selectedObject.transform.position = es.currentSelectedGameObject.gameObject.transform.position;
+        previousBtn = FirstBtn;
+    }
+
+    private void Update()
+    {
+        if (es.currentSelectedGameObject != null)
+        {
+            selectedObject.transform.position = es.currentSelectedGameObject.gameObject.transform.position;
+            previousBtn = es.currentSelectedGameObject.gameObject;
+        }
+        else
+        {
+            es.SetSelectedGameObject(previousBtn);
+            selectedObject.transform.position = es.currentSelectedGameObject.gameObject.transform.position;
+        }
     }
 
     public void LoadLevel(string levelName)
